f72d943516a44517d13c2bd0da84ca25c8a1c665
Bikram Jagdev <64468383+BikramJagdev@users.noreply.github.com>
2021-02-25 21:34:28 +0530

    Final Draft

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.onload = function () {
     // or enable responsive functionality:
-    GitHubCalendar(".calendar", "dinesh-0310", { responsive: true });
+    GitHubCalendar(".calendar", "BikramJagdev", { responsive: true });
   }
