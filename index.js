aab160ffbdd46001c766f17d508458cc126ab0c2
Bikram Jagdev <64468383+BikramJagdev@users.noreply.github.com>
2021-03-08 18:49:34 +0530

    Final

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-window.onload = function () {
-    // or enable responsive functionality:
-    GitHubCalendar(".calendar", "BikramJagdev", { responsive: true });
-  }
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__list');
+
+navToggle.addEventListener('click', () => {
+    console.log("cliked");
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        console.log("cliked");
+        document.body.classList.remove('nav-open');
+    })
+})
